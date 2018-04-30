dd7dd64aaeac82a196715f862a221c6b91070e15
Blake Schmidt <blakecschmidt@gmail.com>
2018-04-29 19:26:01 -0500

    fixed edit items

--- editItem.php
+++ editItem.php
@@ -22,12 +22,13 @@
         </ul>
     </div>
 </header>
-<h1>Delete An Item</h1>
+<div class="editItems">
+<h1>Edit An Item</h1>
 
 <?php
 session_start();
 
-if (isset($_POST)) {
+if (isset($_POST["alertPriceCurrent"])) {
     edit();
 } else {
     editForm();
@@ -61,6 +62,12 @@ function edit()
     $allIDs = $_POST["allIDs"];
     $retailers = $_POST["retailer"];
 
+    $amazonURLOld = $_POST["amazonURLOld"];
+    $bestbuyURLOld = $_POST["bestbuyURLOld"];
+    $dellURLOld = $_POST["dellURLOld"];
+    $walmartURLOld = $_POST["walmartURLOld"];
+    $targetURLOld = $_POST["targetURLOld"];
+
     $amazonURL = $_POST["amazonURL"];
     $bestbuyURL = $_POST["bestbuyURL"];
     $dellURL = $_POST["dellURL"];
@@ -70,63 +77,71 @@ function edit()
     $itemIDsAndURLsToEdit = array();
     $itemRetailersAndURLsToInsert = array();
 
-    if ($_POST["alertPrice"] != "" && $_POST["alertPrice"] != 0) {
+    if (is_numeric($_POST["alertPrice"]) && $_POST["alertPrice"] != 0) {
         $alertPrice = (float) $_POST["alertPrice"];
     } else {
         $alertPrice = (float) $_POST["alertPriceCurrent"];
     }
 
+    if ($alertPrice != (float) $_POST["alertPriceCurrent"]) {
+        foreach ($allIDs as $id) {
+            $stmt = mysqli_prepare($connect, "UPDATE $table_item SET alertPrice = ? WHERE itemID = ?");
+            mysqli_stmt_bind_param($stmt, 'ds', $alertPrice, $id);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_close($stmt);
+        }
+    }
+
     for ($i = 0; $i < sizeof($retailers); $i++) {
-        if ($retailers[$i] == "Amazon" && $amazonURL != "") {
+        if ($retailers[$i] == "Amazon" && $amazonURLOld != "") {
             $itemIDsAndURLsToEdit[] = array($_POST["amazonID"], $amazonURL);
-        } elseif ($retailers[$i] == "Amazon" && $amazonURL == "") {
+        } elseif ($retailers[$i] == "Amazon") {
             $itemRetailersAndURLsToInsert[] = array($retailers[$i], $amazonURL);
-        } elseif ($retailers[$i] == "Best Buy" && $bestbuyURL != "") {
+        } elseif ($retailers[$i] == "Best Buy" && $bestbuyURLOld != "") {
             $itemIDsAndURLsToEdit[] = array($_POST["bestbuyID"], $bestbuyURL);
-        } elseif ($retailers[$i] == "Best Buy" && $bestbuyURL == "") {
+        } elseif ($retailers[$i] == "Best Buy") {
             $itemRetailersAndURLsToInsert[] = array($retailers[$i], $bestbuyURL);
-        } elseif ($retailers[$i] == "Dell" && $dellURL != "") {
+        } elseif ($retailers[$i] == "Dell" && $dellURLOld != "") {
             $itemIDsAndURLsToEdit[] = array($_POST["dellID"], $dellURL);
-        } elseif ($retailers[$i] == "Dell" && $dellURL == "") {
+        } elseif ($retailers[$i] == "Dell") {
             $itemRetailersAndURLsToInsert[] = array($retailers[$i], $dellURL);
-        } elseif ($retailers[$i] == "Walmart" && $walmartURL != "") {
+        } elseif ($retailers[$i] == "Walmart" && $walmartURLOld != "") {
             $itemIDsAndURLsToEdit[] = array($_POST["walmartID"], $walmartURL);
-        } elseif ($retailers[$i] == "Walmart" && $walmartURL == "") {
+        } elseif ($retailers[$i] == "Walmart") {
             $itemRetailersAndURLsToInsert[] = array($retailers[$i], $walmartURL);
-        } elseif ($retailers[$i] == "Target" && $targetURL != "") {
+        } elseif ($retailers[$i] == "Target" && $targetURLOld != "") {
             $itemIDsAndURLsToEdit[] = array($_POST["targetID"], $targetURL);
-        } elseif ($retailers[$i] == "Target" && $targetURL == "") {
+        } elseif ($retailers[$i] == "Target") {
             $itemRetailersAndURLsToInsert[] = array($retailers[$i], $targetURL);
         }
     }
 
     foreach ($itemIDsAndURLsToEdit as $edit) {
-        $stmt1 = mysqli_prepare($connect, "UPDATE $table_item SET alertPrice = (?) WHERE itemID = (?)");
-        mysqli_stmt_bind_param($stmt1, 'ds', $alertPrice, $edit[0]);
+        $stmt1 = mysqli_prepare($connect, "UPDATE $table_itr SET url = ? WHERE itemID = ?");
+        mysqli_stmt_bind_param($stmt1, 'ss', $edit[1],$edit[0]);
         mysqli_stmt_execute($stmt1);
         mysqli_stmt_close($stmt1);
-
-        $stmt2 = mysqli_prepare($connect, "UPDATE $table_itr SET url = (?) WHERE itemID = (?)");
-        mysqli_stmt_bind_param($stmt2, 'ss', $edit[1],$edit[0]);
-        mysqli_stmt_execute($stmt2);
-        mysqli_stmt_close($stmt2);
     }
 
     foreach ($itemRetailersAndURLsToInsert as $insert) {
-        $stmt1 = mysqli_prepare($connect, "INSERT INTO $table_item (itemName, alertPrice) VALUES (?, ?)");
-        mysqli_stmt_bind_param($stmt1, 'sd', $itemName, $alertPrice);
-        mysqli_stmt_execute($stmt1);
-        mysqli_stmt_close($stmt1);
-
-        $stmt2 = mysqli_prepare($connect, "INSERT INTO $table_itu (username) VALUES (?)");
-        mysqli_stmt_bind_param($stmt2, 's', $username);
+        $stmt2 = mysqli_prepare($connect, "INSERT INTO $table_item (itemName, alertPrice) VALUES (?, ?)");
+        mysqli_stmt_bind_param($stmt2, 'sd', $itemName, $alertPrice);
         mysqli_stmt_execute($stmt2);
         mysqli_stmt_close($stmt2);
 
-        $stmt3 = mysqli_prepare($connect, "INSERT INTO $table_itr (retailer, url, currentPrice) VALUES (?, ?, ?)");
-        mysqli_stmt_bind_param($stmt3, 'ssd', $insert[0], $insert[1], NULL);
+        $result_itemID = mysqli_query($connect, "SELECT itemID FROM $table_item ORDER BY itemID DESC");
+        $row = $result_itemID->fetch_row();
+        $itemID = $row[0];
+
+        $stmt3 = mysqli_prepare($connect, "INSERT INTO $table_itu (itemID, username) VALUES (?, ?)");
+        mysqli_stmt_bind_param($stmt3, 'ss', $itemID, $username);
         mysqli_stmt_execute($stmt3);
         mysqli_stmt_close($stmt3);
+
+        $stmt4 = mysqli_prepare($connect, "INSERT INTO $table_itr (itemID, retailer, url) VALUES (?, ?, ?)");
+        mysqli_stmt_bind_param($stmt4, 'sss', $itemID, $insert[0], $insert[1]);
+        mysqli_stmt_execute($stmt4);
+        mysqli_stmt_close($stmt4);
     }
 
     print "<p>The properties you changed have now been edited.</p>";
@@ -159,12 +174,7 @@ function editForm()
         $username = $_SESSION["username"];
     }
 
-    $stmt = mysqli_prepare($connect, "SELECT retailer, url, itemID, alertPrice FROM Item JOIN itemToUser ON Item.itemID = itemToUser.itemID JOIN itemToRetailer ON Item.itemID = itemToRetailer.itemID WHERE username = ? AND itemName = ?");
-    mysqli_stmt_bind_param($stmt, 'ss', $username, $itemName);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
-
-    $result = mysqli_query($connect, $stmt);
+    $result = mysqli_query($connect, "SELECT retailer, url, Item.itemID, alertPrice FROM Item JOIN itemToUser ON Item.itemID = itemToUser.itemID JOIN itemToRetailer ON Item.itemID = itemToRetailer.itemID WHERE username = '" . $username . "' AND itemName = '" . $itemName . "'");
 
     $retailers = array();
     $urls = array();
@@ -182,11 +192,11 @@ function editForm()
     mysqli_close($connect);
 
     $alertPrice = $alertPrices[0];
-    $amazonURL = "";
-    $bestbuyURL = "";
-    $dellURL = "";
-    $walmartURL = "";
-    $targetURL = "";
+    $amazonURLOld = "";
+    $bestbuyURLOld = "";
+    $dellURLOld = "";
+    $walmartURLOld = "";
+    $targetURLOld = "";
     $amazonID = "";
     $bestbuyID = "";
     $dellID = "";
@@ -195,31 +205,29 @@ function editForm()
 
     for ($i = 0; $i < sizeof($retailers); $i++) {
         if ($retailers[$i] == "Amazon") {
-            $amazonURL = $urls[$i];
+            $amazonURLOld = $urls[$i];
             $amazonID = $itemIDs[$i];
         } elseif ($retailers[$i] == "Best Buy") {
-            $bestbuyURL = $urls[$i];
+            $bestbuyURLOld = $urls[$i];
             $bestbuyID = $itemIDs[$i];
         } elseif ($retailers[$i] == "Dell") {
-            $dellURL = $urls[$i];
+            $dellURLOld = $urls[$i];
             $dellID = $itemIDs[$i];
         } elseif ($retailers[$i] == "Walmart") {
-            $walmartURL = $urls[$i];
+            $walmartURLOld = $urls[$i];
             $walmartID = $itemIDs[$i];
         } elseif ($retailers[$i] == "Target") {
-            $targetURL = $urls[$i];
+            $targetURLOld = $urls[$i];
             $targetID = $itemIDs[$i];
         }
     }
 
     print <<<FORM
-<form id = "editForm" method = "post" action = "">
+<p>Please check a retailer box to indicate that you would like to edit the URL or add that listing. Changing the alert price will affect all listings. Checked boxes with blank URLs, blank alert price boxes and alert prices of zero will not be accepted.</p>
+
+<form class="editItemsTable" id = "editForm" method = "post" action = "">
 
 <table>
-
-    <tr>
-        <td>Please check a retailer box to indicate that you would like to edit the URL or add that listing. Changing the alert price will affect all listings. Checked boxes with blank URLs, blank alert price boxes and alert prices of zero will not be accepted.</td>
-    </tr>
 
     <tr>
         <td><label for="itemName">Item Name: </label></td><td><input type="text" name="itemName" id="itemName" placeholder=$itemName readonly></td>
@@ -231,31 +239,39 @@ function editForm()
     
     <tr>
         <td><input type="checkbox" name="retailer[]" value="Amazon" id="amazon"></td><td><label for="amazon"> Amazon</label></td>
-        <td><input type="text" name="amazonURL" id="amazonURL" placeholder=$amazonURL></td>
+        <td><input type="text" name="amazonURL" id="amazonURL" placeholder=$amazonURLOld></td>
     </tr>
     
     <tr>
         <td><input type="checkbox" name="retailer[]" value="Best Buy" id="bestbuy"></td><td><label for="bestbuy"> Best Buy</label></td>
-        <td><input type="text" name="bestbuyURL" id="bestbuyURL" placeholder=$bestbuyURL></td>
+        <td><input type="text" name="bestbuyURL" id="bestbuyURL" placeholder=$bestbuyURLOld></td>
     </tr>
     
     <tr>
         <td><input type="checkbox" name="retailer[]" value="Dell" id="dell"></td><td><label for="dell"> Dell</label></td>
-        <td><input type="text" name="dellURL" id="dellURL" placeholder=$dellURL></td>
+        <td><input type="text" name="dellURL" id="dellURL" placeholder=$dellURLOld></td>
     </tr>
     
     <tr>
         <td><input type="checkbox" name="retailer[]" value="Walmart" id="walmart"></td><td><label for="walmart"> Walmart</label></td>
-        <td><input type="text" name="walmartURL" id="walmartURL" placeholder=$walmartURL></td>
+        <td><input type="text" name="walmartURL" id="walmartURL" placeholder=$walmartURLOld></td>
     </tr>
     
     <tr>
         <td><input type="checkbox" name="retailer[]" value="Target" id="target"></td><td><label for="target"> Target</label></td>
-        <td><input type="text" name="targetURL" id="targetURL" placeholder=$targetURL></td>
+        <td><input type="text" name="targetURL" id="targetURL" placeholder=$targetURLOld></td>
     </tr>
-    
-    <input type="hidden" name="allIDs" id="allIDs" value=$itemIDs>
+FORM;
+
+    foreach ($itemIDs as $id) {
+        print <<<FORM
+<input type="hidden" name="allIDs[]" value="$id">
+FORM;
+
+    }
+    print <<<FORM
     <input type="hidden" name="itemName" id="itemName" value=$itemName>
+    
     <input type="hidden" name="alertPriceCurrent" id="alertPriceCurrent" value=$alertPrice>
     <input type='hidden' name='amazonID' id='amazonID' value=$amazonID>
     <input type='hidden' name='bestbuyID' id='bestbuyID' value=$bestbuyID>
@@ -263,17 +279,19 @@ function editForm()
     <input type='hidden' name='walmartID' id='walmartID' value=$walmartID>
     <input type='hidden' name='targetID' id='targetID' value=$targetID>
     
-    <tr><td><input type="submit" value="Submit"></td></tr>
-    <tr><td><input type="reset" value="Clear"></td></tr>
+    <input type='hidden' name='amazonURLOld' id='amazonURLOld' value=$amazonURLOld>
+    <input type='hidden' name='bestbuyURLOld' id='bestbuyURLOld' value=$bestbuyURLOld>
+    <input type='hidden' name='dellURLOld' id='dellURLOld' value=$dellURLOld>
+    <input type='hidden' name='walmartURLOld' id='walmartURLOld' value=$walmartURLOld>
+    <input type='hidden' name='targetURLOld' id='targetURLOld' value=$targetURLOld>
+    
+    <tr><td><input type="submit" value="Submit"></td><td><input type="reset" value="Clear"></td></tr>
 </table>
 </form>
 FORM;
-
 }
-
-
 ?>
-
+</div>
 <footer>
     <div class="footer">
         <p>Friday, March 23rd, 2018.<br>Website created by Blake Schmidt, Ben Luzarraga, and Kyle Gruber.</p>
